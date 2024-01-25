af8c9df999f171d0570b4c23af654388d797901c
Edson Costa <edson.s@lyncas.net>
2024-01-25 16:06:34 -0300

    Criado um novo componente chamado Progress e nele foram aplicadas as Props e Computed Properties no padrao Composition API, e repassado do componente pai a lista de cursos

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,31 @@
 <script setup>
-import { ref, reactive } from "vue";
+import { reactive } from "vue";
+import Progress from "./components/Progress.vue";
 
-// Ref para tipos primitivos como strings, numeros, booleans, etc.
-let count = ref(0);
-
-// Reactive para tipos compostos como objetos, arrays, etc.
 let courses = reactive([
   {
     title: "Vue.js",
+    done: true,
   },
   {
     title: "React.js",
+    done: false,
   },
   {
     title: "Blazor",
+    done: true,
   },
 ]);
 
-let newCourse = {};
-
-function increment() {
-  count.value++;
-}
+let newCourse = { done: false };
 
 function addCourse() {
   courses.push(newCourse);
-  newCourse = {};
+  newCourse = { done: false };
 }
 </script>
 
 <template>
-  <div>
-    <h1>{{ count }}</h1>
-    <button @click="increment">Incrementar</button>
-  </div>
-
   <div>
     <h1>Lista de cursos</h1>
     <ul>
@@ -43,6 +34,8 @@ function addCourse() {
     <br />
     <input v-model="newCourse.title" type="text" />
     <button @click="addCourse">Adicionar</button>
+
+    <Progress :courses="courses" />
   </div>
 </template>
 
