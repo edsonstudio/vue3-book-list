9b401fa26c56ce9da24a23357381dbd88fb3727f
Edson Costa <edson.s@lyncas.net>
2024-01-26 13:52:22 -0300

    Adicionado novo componente que representara o progresso da leitura dos livros, utilizadas Computed Properties no padrao Composition API

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { reactive } from "vue";
 import Books from "./components/Books.vue";
+import BookProgress from "./components/BookProgress.vue";
 
 let books = reactive([
   {
@@ -60,6 +61,7 @@ function toggleIsRead(id) {
     <div class="books-container">
       <!-- Evento que foi emitido pelo componente filho: @toggleIsRead -->
       <Books @toggleIsRead="toggleIsRead" :books="books" />
+      <BookProgress :books="books" />
     </div>
   </div>
 </template>
